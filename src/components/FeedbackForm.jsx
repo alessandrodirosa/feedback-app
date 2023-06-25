c1064bfd0a57c08ef7299f1b5e0a8dd9f3f6e442
Alessandro Di Rosa <adirosa@deloitte.it>
2023-06-26 01:09:48 +0200

    Completed rating select

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,11 +1,13 @@
 import {Card} from "./shared/Card";
 import {useState} from "react";
 import {Button} from "./shared/Button";
+import {RatingSelect} from "./RatingSelect";
 
 export const FeedbackForm = () => {
     const [text, setText] = useState('');
     const [btnDisabled, setBtnDisabled] = useState(true);
     const [message, setMessage] = useState('');
+    const [rating, setRating] = useState('');
 
     const handleTextChange = (e) => {
         console.log(e.target.value);
@@ -26,6 +28,7 @@ export const FeedbackForm = () => {
         <Card>
             <form>
                 <h2>How would you rate last Favino's movie?</h2>
+                <RatingSelect select={(rating) => setRating(rating)}/>
                 <div className="input-group">
                     <input onChange={handleTextChange} type="text" placeholder="Write a review"/>
                     <Button type="submit" isDisabled={btnDisabled}>Send</Button>
