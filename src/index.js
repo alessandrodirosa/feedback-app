232f49223b74ca0cec0a6c3ca21f1dc2b4b95273
Alessandro Di Rosa <adirosa@deloitte.it>
2023-06-29 00:20:44 +0200

    Removed useless intellij plugin code

--- src/index.js
+++ src/index.js
@@ -2,16 +2,11 @@ import React from "react";
 import {createRoot} from "react-dom/client";
 import App from "./App";
 import "./index.scss";
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
     <React.StrictMode>
-        <DevSupport ComponentPreviews={ComponentPreviews}
-                    useInitialHook={useInitial}>
-            <App/>
-        </DevSupport>
+        <App/>
     </React.StrictMode>
 );
