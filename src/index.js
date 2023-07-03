410000fdd0e42a235bf59804e9b92de5c05fb965
Alessandro Di Rosa <adirosa@deloitte.it>
2023-07-03 18:01:48 +0200

    prova amplify per cucinelli

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import {createRoot} from "react-dom/client";
 import App from "./App";
 import "./index.scss";
 
+//PROVA AMPLIFY
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
