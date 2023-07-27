70d276e9de6beb9dc78f8e5aa067dff0dc453cc9
Alessandro Di Rosa <adirosa@deloitte.it>
2023-07-27 23:38:20 +0200

    Revert "prova amplify per cucinelli"
    This reverts commit 410000fdd0e42a235bf59804e9b92de5c05fb965.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import {createRoot} from "react-dom/client";
 import App from "./App";
 import "./index.scss";
 
-//PROVA AMPLIFY
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
